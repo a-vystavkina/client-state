67dde9805a702c1ff4caba9f05117863cb4c75e6
Alexandra Vystavkina <a.vystavkina@yandex.ru>
2020-01-23 13:17:07 +0300

    task.js fixed

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,22 +1,25 @@
 'use strict';
 
+const elModalClose = document.querySelector('.modal__close');
+const elClose = document.getElementById('subscribe-modal');
 
-const modalClose = document.querySelector('.modal__close');
-const subscribeModal = document.getElementById('subscribe-modal');
 
-document.addEventListener('DOMContentLoaded', function() {
-	let cookie = document.cookie.split(';');
-	console.log(cookie);
-	for (let i = 0; i < cookie.length; i++) {
-		
-		if(cookie[i] === 'name=') {
-			subscribeModal.classList.remove('modal_active');
-		}
-	}
-});
+const getCookie = (e) => {
+  const arrCookie = document.cookie.split("; " + e + "=");
+  return arrCookie;
+}
 
-modalClose.addEventListener('click', function() {
-	subscribeModal.classList.remove('modal_active');
-	document.cookie = 'name=; expires=Tue, 19 Jan 2038 03:14:07 GMT';
-});
+window.onload = function() {    
+    if(getCookie('popup') === 'closed') {        
+        return false;   
+    }
+    
+    elClose.classList.add('modal_active');
 
+    elClose.addEventListener('click', function() {
+        elClose.classList.remove('modal_active');
+        document.cookie = 'popup=closed';         
+    });
+
+    document.cookie = 'popup=closed; Expires=Thu, 01 Jan 1970 00:00:00 GMT';  
+}
